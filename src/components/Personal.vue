ba75d755440be7b21cf9b79afb445ce18f9eb28f
Blake <2979337714@qq.com>
2019-07-06 21:45:53 +0800

    个人页初步

--- src/components/Personal.vue
+++ src/components/Personal.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="wrapper">
     <header></header>
-    <main>个人介绍</main>
+    <main>
+      
+    </main>
+    <div class="touxiang"></div>
+    <div class="message">
+      <ul>
+        <li>Blake Yang</li>
+        <li>求职岗位：前端工程师</li>
+        <li>学历：本科</li>
+        <li>毕业院校：合肥学院</li>
+        <li>专业：网络工程</li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
@@ -11,12 +23,41 @@ export default {
 </script>
 <style lang="scss" scoped>
 .wrapper{
-  width: calc(100% - 300px);
+  width: calc(100% - 250px);
   height: 100vh;
+  background: #f1f2f6;
+  position: relative;
   header{
     width: 100%;
     height: 100px;
-    background: red;
+    background: #a4b0be;
+  }
+  .touxiang{
+    height: 200px;
+    width: 200px;
+    background-image: url(http://img.mp.sohu.com/upload/20170806/72648a7c2e984c8e9da44dc676997692_th.png);
+    background-size: cover;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    margin-top: -60px;
+  }
+  .message{
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    margin-top: 170px;
+    ul{
+      li{
+        width: 200px;
+        text-align: center;
+        margin-top: 20px;
+        font-size: 16px;
+        &:first-child{
+          font-size: 24px;
+        }
+      }
+    }
   }
 }
 </style>
